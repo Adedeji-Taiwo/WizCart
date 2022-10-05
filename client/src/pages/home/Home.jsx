ef7dc5e5c07acf45111e5290c9b0a4f91cefa485
Adedeji Taiwo <dtaiwo45@gmail.com>
2022-10-05 05:42:27 -0700

    ame it better

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import Hero from '../../components/sections/hero/Hero';
 import Service from '../../components/sections/servicee/Service'
 import Brand from '../../components/sections/brands/Brand';
