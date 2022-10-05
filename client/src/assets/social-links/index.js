15a738dab4f9079af9b6af9f87fa6ce4951a7c46
Adedeji Taiwo <dtaiwo45@gmail.com>
2022-10-05 06:32:47 -0700

    make it better

--- client/src/assets/social-links/index.js
+++ client/src/assets/social-links/index.js
@@ -1,12 +1,12 @@
-export { default as facebook } from './Facebook.png';
-export { default as twitter } from './Twitter.png';
-export { default as instagram } from './Instagram.png';
-export { default as pinterest } from './Pinterest.png';
-export { default as youtube } from './Youtube.png';
+export { default as facebook } from './facebook.png';
+export { default as twitter } from './twitter.png';
+export { default as instagram } from './instagram.png';
+export { default as pinterest } from './pinterest.png';
+export { default as youtube } from './youtube.png';
 
 
-export { default as facebookM } from './FacebookM.svg';
-export { default as twitterM } from './TwitterM.svg';
-export { default as instagramM } from './InstagramM.svg';
-export { default as pinterestM } from './PinterestM.svg';
-export { default as youtubeM } from './YoutubeM.svg';
+export { default as facebookM } from './facebookM.svg';
+export { default as twitterM } from './twitterM.svg';
+export { default as instagramM } from './instagramM.svg';
+export { default as pinterestM } from './pinterestM.svg';
+export { default as youtubeM } from './youtubeM.svg';
