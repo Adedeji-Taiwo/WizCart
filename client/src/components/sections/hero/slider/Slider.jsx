15f553a0e9a60d10adc5147f3ee0594f24c12b21
Adedeji Taiwo <dtaiwo45@gmail.com>
2023-01-19 10:02:52 +0100

    added swipecore

--- client/src/components/sections/hero/slider/Slider.jsx
+++ client/src/components/sections/hero/slider/Slider.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination, Navigation,  Autoplay  } from "swiper";
+import SwiperCore, { Pagination, Navigation,  Autoplay  } from "swiper";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
@@ -47,7 +47,7 @@ const data = [
 
 
 const Slider = () => {
-  
+    SwiperCore.use([Autoplay]);
 
   return (
     <Banner>
